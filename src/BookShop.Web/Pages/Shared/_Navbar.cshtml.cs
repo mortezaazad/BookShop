689a50eace2373671bd2883dbbc52d527d13bc6f
Morteza Azad <morteza.azad@outlook.com>
2023-05-02 20:50:16 +0430

    AddAdminLayout

---
+++ src/BookShop.Web/Pages/Shared/_Navbar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookShop.Web.Pages.Shared
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
