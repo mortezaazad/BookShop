d6dc617ff6984539652e79d0643147bde17f2a7e
Morteza <Morteza@DESKTOP-5JNE7JA>
2023-05-08 08:56:46 +0330

    Identity-Bugfix

---
+++ src/BookShop.Web/GlobalUsing.cs
@@ -0,0 +1,1 @@
+﻿global using BookShop.Infrastracture.DataModels;
--- src/BookShop/Infrastracture/ApplicationDbContext.cs
+++ src/BookShop/Infrastracture/ApplicationDbContext.cs
@@ -6,7 +6,7 @@ using System.Reflection.Metadata;
 
 namespace BookShop.Infrastracture
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
