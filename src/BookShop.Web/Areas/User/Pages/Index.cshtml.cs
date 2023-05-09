79515aacc09d4564103b9ba7affe9b3b071c3e13
Morteza <Morteza@DESKTOP-5JNE7JA>
2023-05-09 10:34:03 +0330

    AddUserAreaPages

---
+++ src/BookShop.Web/Areas/User/Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
+
+namespace BookShop.Web.Areas.User.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string? UserName { get; set; }
+        public string UserId { get; set; }
+        public void OnGet()
+        {
+            UserName = User.Identity!.Name;
+            var UserIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+            UserId = UserIdClaim.Value;
+        }
+    }
+}
